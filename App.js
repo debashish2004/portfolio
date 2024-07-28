31053f1a8b124b8fcb4d220c21728e1d2a5e18a6
debashish2004 <115760829+debashish2004@users.noreply.github.com>
2024-07-28 15:04:59 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+<!-- Setup and start animation! -->
+
+    const typed = new Typed ('#element', {
+    strings: ['Web Developer.', '&amp; a Graphic Designer.'],
+    typeSpeed: 50,
+});
